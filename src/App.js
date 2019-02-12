f217f1c448c9243161af24a889b87ee918f0e3e0
Tanmay Patil <tpatil@godaddy.com>
2019-02-11 23:31:03 -0700

    First components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hello
+        <Layout type="desktop" />
       </div>
     );
   }
