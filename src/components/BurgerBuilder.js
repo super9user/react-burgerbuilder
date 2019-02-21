9fca5763a62608ecc2d93c60faafd42fae776d3f
Tanmay Patil <tpatil@godaddy.com>
2019-02-20 20:05:50 -0700

    Rendering burger in preview based on props passed

--- src/components/BurgerBuilder.js
+++ src/components/BurgerBuilder.js
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Preview from './Preview/Preview';
 import { Container, Row, Col } from 'reactstrap';
 
-export default (props) => {
-  return (
-    <Container>
-      <Row className="justify-content-center">
-        <Col sm='6'>
-          <Preview />
-        </Col>
-        <Col sm='6'>
-          <Row className="justify-content-center">
-            <Col xs='auto'>Column2</Col>
-          </Row>
-          <Row className="justify-content-center">
-            <Col xs='auto'>Column3</Col>
-          </Row>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+class BurgerBuilder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      salad: 0,
+      onion: 0,
+      jalapeno: 0,
+      tomato: 0,
+      cheese: 0,
+      patty: 0
+    };
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row className="justify-content-center">
+          <Col sm='6'>
+            <Preview salad={this.state.salad} onion={this.state.onion} 
+            jalapeno={this.state.jalapeno} tomato={this.state.tomato}
+            cheese={this.state.cheese} patty={this.state.patty} />
+          </Col>
+          <Col sm='6'>
+            <Row className="justify-content-center">
+              <Col xs='auto'>Column2</Col>
+            </Row>
+            <Row className="justify-content-center">
+              <Col xs='auto'>Column3</Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    );
+  };
+}
+
+export default BurgerBuilder;
--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -3,32 +3,39 @@ import { Container, Row, Col } from 'reactstrap';
 import assets from '../../assets/burger-composition';
 
 export default (props) => {
+  const salads = getIngredientsRows(props.salad, assets.salad),
+    onions = getIngredientsRows(props.onion, assets.onion),
+    jalapenos = getIngredientsRows(props.jalapeno, assets.jalapeno),
+    tomatoes = getIngredientsRows(props.tomato, assets.tomato),
+    cheese = getIngredientsRows(props.cheese, assets.cheese),
+    patties = getIngredientsRows(props.patty, assets.patty);
+
   return (
     <Container>
       <Row>
         <Col><img className='mx-auto d-block' src={assets.topBun} alt="" /></Col>
       </Row>
-      <Row>
-        <Col><img className='mx-auto d-block' src={assets.salad} alt="" /></Col>
-      </Row>
-      <Row>
-        <Col><img className='mx-auto d-block' src={assets.onion} alt="" /></Col>
-      </Row>
-      <Row>
-        <Col><img className='mx-auto d-block' src={assets.jalapeno} alt="" /></Col>
-      </Row>
-      <Row>
-        <Col><img className='mx-auto d-block' src={assets.tomato} alt="" /></Col>
-      </Row>
-      <Row>
-        <Col><img className='mx-auto d-block' src={assets.cheese} alt="" /></Col>
-      </Row>
-      <Row>
-        <Col><img className='mx-auto d-block' src={assets.patty} alt="" /></Col>
-      </Row>
+      {salads}
+      {onions}
+      {jalapenos}
+      {tomatoes}
+      {cheese}
+      {patties}
       <Row>
         <Col><img className='mx-auto d-block' src={assets.bottomBun} alt="" /></Col>
       </Row>
     </Container>
   );
 };
+
+function getIngredientsRows(iterateLength, assetSrc) {
+  const ingredient = [];
+  for (let i = 0; i < iterateLength; i++) {
+    ingredient.push((
+      <Row>
+        <Col><img className='mx-auto d-block' src={assetSrc} alt="" /></Col>
+      </Row>
+    ));
+  }
+  return ingredient;
+}
