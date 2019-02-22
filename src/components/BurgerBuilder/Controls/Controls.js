e851f15dc01d515f9a5c250d6eed88b1c24e8ead
Tanmay Patil <tpatil@godaddy.com>
2019-02-21 20:03:04 -0700

    Perf optimization

--- src/components/BurgerBuilder/Controls/Controls.js
+++ src/components/BurgerBuilder/Controls/Controls.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { Form, Alert } from 'reactstrap';
 import IngredientControl from './IngredientControl';
 
-export default (props) => {
-  return (
-    <div className="shadow p-3 mb-5 bg-white rounded">
-      <Form>
-        <Alert color="info">Select your Burger ingredients</Alert>
-        <IngredientControl ingredient="salad" onChangeHandler={props.onChangeHandler} />
-        <IngredientControl ingredient="onion" onChangeHandler={props.onChangeHandler} />
-        <IngredientControl ingredient="jalapeno" displayName="jalapenos" onChangeHandler={props.onChangeHandler} />
-        <IngredientControl ingredient="tomato" displayName="tomatoes" onChangeHandler={props.onChangeHandler} />
-        <IngredientControl ingredient="cheese" onChangeHandler={props.onChangeHandler} />
-        <IngredientControl ingredient="patty" displayName="patties" onChangeHandler={props.onChangeHandler} />
-      </Form>
-    </div>
-  )
-};
+class Controls extends PureComponent {
+
+  render() {
+    return (
+      <div className="shadow p-3 mb-5 bg-white rounded">
+        <Form>
+          <Alert color="info">Select your Burger ingredients</Alert>
+          <IngredientControl ingredient="salad" onChangeHandler={this.props.onChangeHandler} />
+          <IngredientControl ingredient="onion" onChangeHandler={this.props.onChangeHandler} />
+          <IngredientControl ingredient="jalapeno" displayName="jalapenos" onChangeHandler={this.props.onChangeHandler} />
+          <IngredientControl ingredient="tomato" displayName="tomatoes" onChangeHandler={this.props.onChangeHandler} />
+          <IngredientControl ingredient="cheese" onChangeHandler={this.props.onChangeHandler} />
+          <IngredientControl ingredient="patty" displayName="patties" onChangeHandler={this.props.onChangeHandler} />
+        </Form>
+      </div>
+    )
+  }
+}
+
+export default Controls;
